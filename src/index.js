65aa478adfc304a1d60e16b8f95866df754bd6ca
Joshua <joshsmayhew@gmail.com>
2022-07-11 15:41:36 +0700

    create function that fetches dog breeds from api assigned to variable and returns an object by which breeds are accessed as keys and returned via object.keys method and appended to Dom

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const dogContainer = document.querySelector("#dog-image-container");
 const makeUl = document.createElement("ul");
+const ulBreedContainer = document.querySelector("#dog-breeds");
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("%c HI", "color: firebrick");
+    console.log("%c HI", "color: firebrick");
 
-  fetchDogPictures();
+    fetchDogPictures();
+    fetchDogBreeds();
+    ulBreedContainer.addEventListener("click", handleClick());
 });
+
+
+// ASYNC Functions and JSON
 
 // retrives dog img files as json and parses it into object literal
 
@@ -24,7 +30,19 @@ const fetchDogPictures = () => {
 
 const breedUrl = 'https://dog.ceo/api/breeds/list/all';
 
+    const fetchDogBreeds = () => {
+        fetch (breedUrl)
+        .then((resp) => resp.json())
+        .then((data) => {
+            console.log(data);
+            Object.keys(data.message).forEach((key) => {
+                createEl(key);
+            });
+        })
+    }
 
+
+// DOM Manipulations
 
 
 // Makes Img element to be appended to DOM
@@ -35,13 +53,36 @@ function makeEl(img) {
 
   let newLi = document.createElement("li");
 
-  appendEl(newImg, newLi);
+  appendEl(newImg, newLi, "dog-image-container");
 }
 
+
+// makes element from dog breed data that is appeneded to the DOM
+
+    function createEl(objKey) {
+        let newListItem = document.createElement("li");
+        newListItem.classList.add("breed-list");
+        newListItem.textContent = objKey;
+
+        appendEl("", newListItem, "dog-breeds");
+
+    }
 // function that appends any elements passed to it to the dog container of document
 
-function appendEl(el1, el2) {
+function appendEl(el1, el2, el3) {
   el2.append(el1);
 
-  document.getElementById("dog-image-container").append(el2);
+  document.getElementById(el3).append(el2);
 }
+
+// Event Listeners
+
+
+
+function handleClick() {
+   console.log("test");
+}
+
+
+
+
